5ce14c4a67c955c3710b7162817fb0bfc634b93f
Abigail Gray <abbie1@gmail.com>
2018-12-08 20:51:56 +0000

    Initial Commit - based on Hello World Java Spring Boot example

---
+++ petOfTheDay/src/main/java/hello/HelloWorldController.java
@@ -0,0 +1,31 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/hello-world")
+    @ResponseBody
+    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Abigail") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+
+    @GetMapping("/pet")
+    @ResponseBody
+    public Greeting sayHelloFido(@RequestParam(name="name", required=false, defaultValue="Fido") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+
+
+}
